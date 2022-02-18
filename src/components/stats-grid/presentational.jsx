c6b4f814e6d7be16ab384fa3d963c51e789b9336
Juan Pablo Rombola <juampi.rombola@gmail.com>
2022-02-18 19:12:28 -0300

    Remove unused variable

--- src/components/stats-grid/presentational.jsx
+++ src/components/stats-grid/presentational.jsx
@@ -26,7 +26,7 @@ import {
 } from "./statsKeys";
 
 const StatsGrid = () => {
-    const {data, loading} = useContext(RoninContext)
+    const {data} = useContext(RoninContext)
 
     return (
         <Container maxW='container.lg'>
